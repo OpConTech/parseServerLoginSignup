d4b28ea4bea2e6a1104f72dd40ddc9fca492ef84
Tom Neary <mail@opcontech.com>
2016-05-20 16:07:48 -0700

    Initial Commit

---
+++ parseServerLoginSignup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  parseServerLoginSignup
+//
+//  Created by Thomas Neary on 5/20/16.
+//  Copyright © 2016 OpCon Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ parseServerLoginSignup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  parseServerLoginSignup
+//
+//  Created by Thomas Neary on 5/20/16.
+//  Copyright © 2016 OpCon Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
